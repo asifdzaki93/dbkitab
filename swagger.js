4eb1539610aa271bcea782414797285b66bd20d1
asifdzakiy-droid <asifdzakiy@gmail.com>
2026-07-10 04:31:16 +0700

    fix: update swagger ui for vercel

--- swagger.js
+++ swagger.js
@@ -32,8 +32,13 @@ const options = {
 const swaggerSpec = swaggerJSDoc(options);
 
 function setupSwagger(app) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log('Swagger UI available at http://localhost:8081/api-docs');
+  const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+  app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL })
+  );
+  console.log('Swagger UI available at /api-docs');
 }
 
 module.exports = setupSwagger;
